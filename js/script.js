d92cac010f9e9c2690df7ca22f91bd0113ef113f
bruks <jenake8@gmail.com>
2022-04-29 15:05:17 +0100

    mobile nav animation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const search_btn = document.getElementsByClassName("fa-magnifying-glass")[0]
 const search_con = document.getElementsByClassName("search-container")[0]
 const xbtn = document.getElementsByClassName("fa-x")[0]
 const link_top = document.getElementById("link-top")
+const mobile_bar = document.getElementsByClassName("mobile-bar")[0]
+const mobile_links = document.getElementsByClassName("mobile-links")[0]
 
 function toggleSearchCon(){
     var list = link_top.children
@@ -27,3 +29,23 @@ function toggleSearchCon(){
 
 search_btn.addEventListener("click", toggleSearchCon)
 xbtn.addEventListener("click", toggleSearchCon)
+
+//smart phone
+function toggle_mobile_nav(){
+    if (mobile_links.style.height != "300px"){
+        mobile_links.style.transition ="0.5s"
+        mobile_links.style.height = "300px"
+        this.className = "fa-solid fa-x"
+        this.parentElement.style.color =  "white"
+        this.parentElement.style.backgroundColor = "rgb(51, 51, 255)"
+    }
+    else{
+        mobile_links.style.height = "0px"
+        this.className = "fa-solid fa-bars mobile-bar"
+        this.parentElement.style.transition = "0.5s"
+        this.parentElement.style.color =  "rgb(51, 51, 255)"
+        this.parentElement.style.backgroundColor = "rgb(236, 242, 249)"
+    }
+}
+
+mobile_bar.addEventListener("click",toggle_mobile_nav)
